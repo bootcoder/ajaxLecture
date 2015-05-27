53ecc48dad9919ce1e4b2bc5fdc4c62428afea9d
BootCoder <bootcoder@gmail.com>
2015-05-27 12:20:19 -0700

    complete migrations and seed file

--- db/migrate/20150527102950_create_pictures.rb
+++ db/migrate/20150527102950_create_pictures.rb
@@ -1,4 +1,14 @@
 class CreatePictures < ActiveRecord::Migration
   def change
+    create_table :pictures do |t|
+
+      t.string :img_url
+      t.string :caption
+
+      t.references :user
+
+      t.timestamps
+
+    end
   end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+user1 = User.create(username: "TomTom", email: "tom@tom.com")
+picture1 = Picture.create(img_url: "http://gph.is/1PNGiUo")
