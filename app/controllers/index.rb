16431fc819d2f82b96559d7a96552bdffefa2aa6
BootCoder <bootcoder@gmail.com>
2015-05-27 14:41:46 -0700

    comment seedDB logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,14 @@
+require 'json'
+
 get '/' do
   erb :index
 end
+
+# post '/pictures' do
+#   p "*" * 90
+#   p params
+#   p "*" * 90
+#   parsed = json.parse(params)
+#   p parsed
+
+# end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,81 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  console.log("page loaded!");
+
+  // seedButtonListener();
+  gifButtonListener();
+
 });
+
+
+// var seedButtonListener = function() {
+
+//   $('#seedBTN').on('click', function(){
+
+//     var allImgs = $('.gif_img');
+//     var dataToSend = []
+//     var arrayLength = allImgs.length
+
+//     for(var i = 0; i < arrayLength; i += 1) {
+//       dataToSend.push(allImgs[i].src)
+//     }
+
+//     // console.log(dataToSend)
+
+//     var request = $.ajax({
+//       type: 'POST',
+//       url: '/pictures',
+//       data: {'imgs': JSON.stringify(dataToSend)}
+//     })
+
+//     request.done(function(response){
+//       console.log(response);
+//       console.log('There and back again...Yay!')
+//     });
+
+
+//     request.fail(function(response){
+//       console.log(response);
+//       console.log('I have failed at all the things...')
+//     });
+
+//   })
+// }
+
+
+var gifButtonListener = function() {
+
+  $('#gifBTN').on('click', function(event){
+    event.preventDefault();
+
+    var request = $.ajax({
+      type: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/trending',
+      data: {"api_key": "dc6zaTOxFJmzC"}
+    })
+
+    request.done(function(response){
+
+      console.log('YAY');
+      console.log(arguments);
+
+      // Work with response data to get the thing you want.
+
+      // Just some code for selecting a random array element.
+      var rand = response.data[Math.floor(Math.random() * response.data.length)];
+
+      console.log(rand.images);
+      var img = rand.images.fixed_width.url
+
+      // Actual DOM manipulation.
+      $('.gif_container').append('<img class="gif_img" src='+ img +'>')
+
+    });
+
+    request.fail(function(response){
+      console.log("ohhh noooooo");
+    });
+
+  });
+
+}
